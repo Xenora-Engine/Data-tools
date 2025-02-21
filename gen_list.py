5d5045ba5df1e8f18c74a0d285549b3a51c5069a
XIFIYT <xifishopng@gmail.com>
2025-02-21 10:17:31 +0100

    fix: rename

--- gen_list.py
+++ gen_list.py
@@ -1,8 +1,8 @@
 def format_list_as_struct(lst, struct_name="transaction", var_name="all_transactions", double_field=False):
-    struct_name = input("Entrez le nom de la struct : ").strip() or struct_name
-    var_name = input("Entrez le nom de la liste (variable) : ").strip() or var_name
+    struct_name = input("Enter the name of the struct : ").strip() or struct_name
+    var_name = input("Enter the name of list : ").strip() or var_name
     
-    print("Veuillez entrer les noms des champs pour la struct (séparés par des virgules) :")
+    print("Enter the argument for the struct :")
     fields = input().strip().split(',')
     
     struct_def = "struct " + struct_name + " {\n    " + ";\n    ".join(f"std::string {field.strip()}" for field in fields) + ";\n};\n\n"
@@ -23,11 +23,3 @@ def format_list_as_struct(lst, struct_name="transaction", var_name="all_transact
     formatted_output = struct_def + 'std::vector<' + struct_name + '> ' + var_name + ' = {\n    ' + ',\n    '.join(formatted_items) + '\n};'
     
     return formatted_output
-
-with open("list/input.txt", "r", encoding="utf-8") as file:
-    transactions = [tuple(line.strip().split(',')) for line in file if line.strip()]
-
-formatted_output = format_list_as_struct(transactions).replace('""', '"')
-
-with open("list/output.txt", "w", encoding="utf-8") as file:
-    file.write(formatted_output)
